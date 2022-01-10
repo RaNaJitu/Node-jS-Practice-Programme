1651c7de16eeeaa7cf88605cd186bee307190c73
RaNaJitu <jitendrashivmendir@gmail.com>
2022-01-10 16:33:23 +0530

    Node Js Practices

---
+++ firstNodeFile.js
@@ -0,0 +1,45 @@
+//console.log("jitu")
+
+// const a = 10;
+// const b = 20;
+// c = a+ b;
+// console.log("sum = "+c)
+
+// TODO ***********object topics***********
+// const user = {
+//     name:"jitu",
+//     age :25,
+//     email :"ranajeetsst@gmail.com"
+// }
+
+// console.log(user);
+
+// ! ************Anonymous Function****************
+const person = function(){
+    console.log("Anonymous Function");
+}
+
+//  **********Arrow Function****************
+// const arrowPerson = ()=>{
+//     console.log("Arrow Function");
+// }
+ person();
+// arrowPerson();
+
+// TODO Global Objects in node js
+
+/*
+__dirname
+__filename
+require
+console
+buffer
+module
+exports
+*/
+//console.log(__dirname) // ! its return the directory name
+//console.log(__filename) // todo returns the file name
+//const fileName = require('./firstNodeFile.js'); //? its is used to access the different file where u wants
+//var name = "jitu"
+ //module.exports.abs =name; //? this is use for set the values to access this variable to others files
+ //console.log(fileName.abs)//! this is use for access the data from others fille
